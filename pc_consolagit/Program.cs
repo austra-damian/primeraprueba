bfbbd0c57cefbd924b27352a5076e2673eef9c46
Damian Osses <damian.osses@austranet.com>
2017-10-30 12:33:02 -0300

    Se agregan campos

--- pc_consolagit/Program.cs
+++ pc_consolagit/Program.cs
@@ -10,6 +10,32 @@ namespace pc_consolagit
     {
         static void Main(string[] args)
         {
+            String accion = "nada";
+            int aventura = 1;
+
+            while (aventura > 0)
+            {
+                Console.WriteLine("Te encuentras en una sala completamente oscura, cuando tus ojos se adaptan a la ausencia de luz");
+                accion = Console.ReadLine();
+                switch (accion.ToLower())
+                {
+                    case "mirar":
+                        Console.WriteLine("Miras a tu alrededor, ");
+                        aventura = 0;
+                        break;
+                    case "matarse":
+                        Console.WriteLine("Te matas");
+                        aventura = 0;
+                        break;
+                    default:
+                        Console.WriteLine("No puedes " + accion + " en este momoento");
+                        aventura = 0;
+                        break;
+                }
+            }
+
+            Console.WriteLine("Gracias por jugar.");
+            Console.ReadKey();
         }
     }
 }
