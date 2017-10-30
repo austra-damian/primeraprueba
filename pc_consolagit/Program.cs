d8d580e4d754f64313ba8d79faca84f0c25ace3e
Damian Osses <damian.osses@austranet.com>
2017-10-30 12:17:28 -0300

    Agregar archivos de proyecto.

---
+++ pc_consolagit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pc_consolagit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
